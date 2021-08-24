30b43c6924d34d4e20b418328bedc2a8b0e3f8da
= <=>
2021-08-24 10:31:48 -0300

    Primeiro commit: criacao do projeto

---
+++ src/main/java/com/example/bookmaneger/BookmanegerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmaneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanegerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmanegerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookmaneger/BookmanegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmaneger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanegerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
